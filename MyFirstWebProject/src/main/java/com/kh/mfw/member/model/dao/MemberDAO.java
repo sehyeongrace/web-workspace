e9fcdbad5690c885e9352c68b314a5957680c1eb
sehyeongrace <sehyeon.grace@gmail.com>
2025-03-12 13:59:17 +0900

    remove cached

---
+++ MyFirstWebProject/src/main/java/com/kh/mfw/member/model/dao/MemberDAO.java
@@ -0,0 +1,21 @@
+package com.kh.mfw.member.model.dao;
+
+import com.kh.mfw.member.model.dto.MemberDTO;
+
+public class MemberDAO {
+		
+	static {
+		
+			Class.forName("");
+		
+	}
+	
+	public void login(MemberDTO member) {
+		
+		
+		
+		
+		
+	}
+	
+}
